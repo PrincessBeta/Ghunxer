fe3c6257f5e628338cfb8ac542b0c9a57967e200
PrincessBeta <princessbranlobein@pm.me>
2023-01-26 14:48:26 +0100

    character sheet creator added

--- ghunxer.py
+++ ghunxer.py
@@ -1,6 +1,7 @@
 
 import interactions
 from random import randrange
+import csv
 
 
 SERVER_ID = 830127177869426699
@@ -66,50 +67,28 @@ async def roll(ctx,dice: str,bonus: int = 0,malus: int = 0):
     if malus : message+=f" + {malus}"
     await ctx.send(message)
 
-# def roll_all_dice(string :str):
-#     pattern = re.compile("[0-9]+d[0-9]+")
-#     dice = pattern.findall(string)
-#     rolls = []
-#     for roll in dice:
-#         n,die = roll.split("d")
-#         n,die = int(n),int(die)
-#         value = 0
-#         for i in range(n):
-#             value+=randrange(1,die+1)
-#         rolls.append(value) 
-    
-#     for die in range(len(dice)):
-#         string = string.replace(dice[die],str(rolls[die]))
-#     return string
-
-
-# @bot.command(
-#     name="my_first_command",
-#     description="This is the first command I made!"
-#     )
-# async def my_first_command(ctx: interactions.CommandContext):
-#     await ctx.send("Hi there!")
-
 #-----------------------------------------------------------------------------#
 #                              CSV manipulation                               #
 #-----------------------------------------------------------------------------#
 
-# def csv_add_row(filename : str,x : list) -> bool:
-#     """
-#     adds a row to a csv file if it isn't already in it.
-#     returns true if the row was added, false if not.
-#     """
-#     with open(filename,"a+",encoding="utf-8",newline= "") as file:
-#         file.seek(0) #we need to put the file pointer back at the start
-#         reader = csv.reader(file,delimiter=",", quotechar='"')
-#         writer = csv.writer(file,delimiter=",", quotechar='"')
+def create_character_sheet(
+    name:str,
+    user:str,
+    channel:str,
+    bank:int,
+    stats:list,
+    inventory:dict={}
+    ) :
+    with open(f"characters/{name}.csv","x") as sheet :
+        stats_string = " ".join([str(k) for k in stats])
+        sheet.write(name+"\n"+user+"\n"+channel+"\n"+str(bank)+"\n"+stats_string)
 
-#         #only write if sender's not already written
-#         if not list(map(str,x)) in reader:
-#             writer.writerow(x)
-#             return True
-#         return False
+        for item in inventory.keys() :
+            sheet.write("\n"+item + ":" + str(inventory[item]))
+        
 
+        
+create_character_sheet("naruto","123","321",50,[1,2,3,4],{"saucisse":12,"tomate":14,"amour propre" : 0})
 #-----------------------------------------------------------------------------#
 #                             Launching the bot                               #
 #-----------------------------------------------------------------------------#
